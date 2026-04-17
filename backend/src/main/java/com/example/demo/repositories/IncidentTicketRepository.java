8dacec0817cf122c4afcf450f254bffaa411c8b8
SasiRanasinghe <sasinisathsarani33@gmail.com>
2026-04-17 12:56:50 +0530

    create ticket dto and repository

---
+++ backend/src/main/java/com/example/demo/repositories/IncidentTicketRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.IncidentTicket;
+import com.example.demo.models.TicketStatus;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IncidentTicketRepository extends MongoRepository<IncidentTicket, String> {
+    List<IncidentTicket> findByCreatedBy(String createdBy);
+    List<IncidentTicket> findByAssignedTechnician(String assignedTechnician);
+    List<IncidentTicket> findByStatus(TicketStatus status);
+    List<IncidentTicket> findByCreatedByAndStatus(String createdBy, TicketStatus status);
+}
